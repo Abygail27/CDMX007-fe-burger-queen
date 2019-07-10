8042464b5725796217c9df63f01415714c7fc27b
Abygail27 <abygaailrrocha@gmail.com>
2019-07-09 19:40:32 -0500

    rendering image in card

--- src/components/headerContent/orders.js
+++ src/components/headerContent/orders.js
@@ -1,6 +1,8 @@
 import  React, { Component } from 'react'
 
 
+const FoodData = require('../data/FoodData.json');
+
 
  class OrderCard extends Component {
     render() {
@@ -9,6 +11,20 @@ import  React, { Component } from 'react'
                   <div className="row">
     <div className="col s12 m6">
       <div className="card blue-grey darken-1">
+      <div class="card-content white-text">
+
+      {FoodData.map((id, index)=>{
+           return <img className="img-card" src={id.content} height = "77px" alt=""></img>
+         })}
+
+      <div class="card-action">
+         {FoodData.map((id, index)=>{
+           return <span>{id.title}</span>
+         })}
+         
+        </div>
+
+        </div>
         <div className="card-content white-text">
          
         </div>
